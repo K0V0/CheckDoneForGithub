711bf25b70256fc0f51681c91bffa0ee04b531e5
Matej Kováč <kovo.mato@gmail.com>
2020-12-15 03:19:21 +0100

    adding writing notes functionallity - frontend/visual part done

--- js/content.js
+++ js/content.js
@@ -4,10 +4,13 @@ nema pristup k html rozsirenia samotneho (popup)
 */
 
 function Content() {
+	this.autosaveTimeDelay = 1000;
 	this.FX = new ContentFunctions();
 	this.STORE;
 	this.mdContainer;
 	this.listItems;
+	this.addNoteButtons;
+	this.noteAreas;
 	this.init();
 }
 
@@ -37,13 +40,16 @@ Content.prototype = {
 
 		// draw checkbox with belonging state according to data from store
 		totok.mdContainer.find('li').each(function() {
+			totok.FX.appendNoteButton($(this));
 			totok.FX.appendCheckbox(
 				$(this), 
 				totok.STORE.isCheckboxEnabled($(this).attr('id'))
 			);
 		});
 
-		totok.listItems = totok.mdContainer.find('input[type=checkbox].git-taskchecker-checkbox')
+		totok.listItems = totok.mdContainer.find('input[type=checkbox].git-taskchecker-checkbox');
+		totok.addNoteButtons = totok.mdContainer.find('a.git-taskchecker-add-note');
+		totok.noteAreas = totok.mdContainer.find('textarea.git-taskchecker-note-area'); // element appended by user action too
 
 		totok.listItems.on('click', function() {
 			var	scope_ancestors = $(this).siblings().find('input[type=checkbox].git-taskchecker-checkbox');
@@ -54,6 +60,11 @@ Content.prototype = {
 			scope_ancestors.trigger('change'); // needs be triggered if changed programatically
 		});
 
+		totok.addNoteButtons.on('click', function(e) {
+			e.preventDefault();
+			totok.FX.appendNoteArea($(this).parent('li'));
+		});
+
 		// reaguje aj na programaticku zmenu
 		totok.listItems.on('change', function() { 
 			totok.STORE.setCheckbox(
@@ -62,12 +73,22 @@ Content.prototype = {
 			);
 			totok.STORE.commit();
 		});
+
+		// do not work on elements appended by user action(s)
+		/*totok.noteAreas.on('change', function() {
+			console.log("idee");
+		});*/
+
+		$(document).frequentFireLimit('input', totok.autosaveTimeDelay, 'textarea.git-taskchecker-note-area', function() {
+			console.log("ideffffffe");
+		});
 	},
 
 	removeCheckboxes: function() {
 		// means removes only elements from page, data of checkboxes are persisted
 		// data are persisted if in future turn then user on
 		$(document).find('input.git-taskchecker-checkbox').remove();
+		$(document).find('a.git-taskchecker-add-note').remove();
 	},
 
 	resetCheckboxes: function() {
--- js/content_functions.js
+++ js/content_functions.js
@@ -17,15 +17,26 @@ ContentFunctions.prototype = {
 	},
 
 	appendCheckbox: function(elem, checked) {
-		var prep_elem = $('<input type="checkbox" id="task-' + elem.attr('id') + '" class="git-taskchecker-checkbox">');
+		var prep_elem = $('<input type="checkbox" id="task-' + elem.attr('id') + '" class="git-taskchecker-checkbox" alt="CheckBox" title="CheckBox">');
 		if (checked === true) {
 			// do not add prop when false, children items in list then not manipulable by parent
 			//prep_elem.prop('checked', checked);
 			// attr better
 			prep_elem.attr('checked', checked);
 		}
-		elem.css({ 'position':'relative' });
+		//elem.css({ 'position':'relative' });
 		elem.prepend(prep_elem);
+	},
+
+	appendNoteButton: function(elem) {
+		var prep_elem = $('<a class="git-taskchecker-add-note" id="addnote-' + elem.attr('id') + '" href="#" alt="Add Note(s)" title="Add Note(s)">+</a>');
+		elem.prepend(prep_elem);
+	},
+
+	appendNoteArea: function(elem) {
+		var app_elem = $('<textarea class="git-taskchecker-note-area"></textarea>');
+		elem.append('<br>');
+		elem.append(app_elem);
 	}
 
 }
